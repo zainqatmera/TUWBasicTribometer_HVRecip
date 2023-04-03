91bf26f3be452312a4a27af8c7a128cd713aee72
sm <s.medina@btinternet.com>
2023-04-03 14:13:27 +0100

    Added Vertical Recip View and Model

---
+++ TUWBasicTribometer_HVRecip/ViewModels/VerticalRecipViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUWBasicTribometer_HVRecip.ViewModels
+{
+    internal class VerticalRecipViewModel : BindableBase
+    {
+
+
+        private int _upperPosition;
+        public int UpperPosition
+        {
+            get => _upperPosition;
+            set => SetProperty(ref _upperPosition, value);
+        }
+
+        private int _lowerPosition;
+        public int LowerPosition { 
+            get => _lowerPosition;
+            set => SetProperty(ref _lowerPosition, value);
+        }
+
+    }
+}
